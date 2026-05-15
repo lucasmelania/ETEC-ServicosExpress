922506433d7eb47d7c3608c0fbc2dae01e60a927
lucasmelania <contact.lucasmelania@gmail.com>
2026-05-15 14:38:47 -0300

    primeiro commit

--- bd/servicos.js
+++ bd/servicos.js
@@ -11,4 +11,42 @@ function listarPorCategoria(categoria){
     return produtosFiltrados
 }
 
-export {listarProdutos, listarPorCategoria}
+function calcularEstoqueProduto(id) {
+   
+    //Procurar Produto por id
+    const produto = produtos.find(produto => produto.id == id)
+
+    // Retornar quanto de dinheito a em estoque
+    
+    return produto.preco * produto.quantidade
+
+}
+
+function listarPorId(id) {
+    
+    //Procurar Produto por id
+    const produto = produtos.find(produto => produto.id == id)
+
+    // Listar Produto
+
+    return produto
+
+
+}
+
+function listarPorNome(nome){
+
+    //Procurar Produto por nome
+    
+    const produto = produtos.filter(produto =>
+        produto.nome.toLowerCase().includes(nome.toLowerCase())
+    )
+
+    
+    // Listar Produto
+
+    return produto
+
+}
+
+export {listarProdutos, listarPorCategoria, calcularEstoqueProduto, listarPorId, listarPorNome}
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 
-import { listarProdutos, listarPorCategoria } from './bd/servicos.js'
+import { listarProdutos, listarPorCategoria, calcularEstoqueProduto, listarPorId, listarPorNome } from './bd/servicos.js'
 
 
 const app = express()
@@ -19,6 +19,33 @@ app.get('/produtosCategoria/:categoria', (req, res) => {
    let produtosFiltrados = listarPorCategoria(categoria)
    res.json(produtosFiltrados)
 })
+
+app.get('/estoque/:id', (req, res) => {
+
+  let { id } = req.params
+  let valorEstoque = calcularEstoqueProduto(id)
+  res.json({
+      valorEstoque
+  })
+
+})
+
+app.get('/produtoId/:id', (req, res) => {
+
+  let { id } = req.params
+  let produto = listarPorId(id)
+  res.json(produto)
+
+})
+
+app.get('/produtoNome/:nome', (req, res) => {
+
+  let { nome } = req.params
+  let produto = listarPorNome (nome)
+  res.json(produto)
+
+})
+
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000')
