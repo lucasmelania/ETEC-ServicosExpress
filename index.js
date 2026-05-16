8993f5b0f07e24d7869833f30708e34a4e37d2c4
lucasmelania <contact.lucasmelania@gmail.com>
2026-05-15 23:51:12 -0300

    9 commit

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
 import {
   listarProdutos,
@@ -12,9 +14,20 @@ import {
   valorPorCategoria,
 } from "./bd/servicos.js";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
-app.use(express.static("public"));
+// Servir arquivos estáticos
+app.use(express.static(path.join(__dirname, "public")));
+
+// Rota raiz para o index.html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+
 
 app.get("/produtos", (req, res) => {
   let produtos = listarProdutos();
