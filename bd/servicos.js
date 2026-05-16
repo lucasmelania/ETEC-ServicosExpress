2c3bfb55794c77699a18302c8924b9ab6a593265
lucasmelania <contact.lucasmelania@gmail.com>
2026-05-15 22:45:52 -0300

    2 commit

--- bd/servicos.js
+++ bd/servicos.js
@@ -1,52 +1,128 @@
 import { produtos } from "./bd.js";
 
-function listarProdutos(){
-    return produtos
+function listarProdutos() {
+  return produtos;
 }
 
-function listarPorCategoria(categoria){
-    const produtosFiltrados = produtos.filter(produto => 
-        produto.categoria === categoria
-    );
-    return produtosFiltrados
+function listarPorCategoria(categoria) {
+  const produtosFiltrados = produtos.filter(
+    (produto) => produto.categoria === categoria,
+  );
+  return produtosFiltrados;
 }
 
 function calcularEstoqueProduto(id) {
-   
-    //Procurar Produto por id
-    const produto = produtos.find(produto => produto.id == id)
+  //Procurar Produto por id
+  const produto = produtos.find((produto) => produto.id == id);
 
-    // Retornar quanto de dinheito a em estoque
-    
-    return produto.preco * produto.quantidade
+  // Retornar quanto de dinheito a em estoque
 
+  return produto.preco * produto.quantidade;
 }
 
 function listarPorId(id) {
-    
-    //Procurar Produto por id
-    const produto = produtos.find(produto => produto.id == id)
+  //Procurar Produto por id
+  const produto = produtos.find((produto) => produto.id == id);
 
-    // Listar Produto
+  // Listar Produto
 
-    return produto
-
-
+  return produto;
 }
 
-function listarPorNome(nome){
+function listarPorNome(nome) {
+  //Procurar Produto por nome
 
-    //Procurar Produto por nome
-    
-    const produto = produtos.filter(produto =>
-        produto.nome.toLowerCase().includes(nome.toLowerCase())
-    )
+  const produto = produtos.filter((produto) =>
+    produto.nome.toLowerCase().includes(nome.toLowerCase()),
+  );
 
-    
-    // Listar Produto
+  // Listar Produto
 
-    return produto
-
+  return produto;
 }
 
-export {listarProdutos, listarPorCategoria, calcularEstoqueProduto, listarPorId, listarPorNome}
+function listarEstoqueBaixo() {
+  //Filtrar por quantidade
+  const produtosFiltrados = produtos.filter(
+    (produto) => produto.quantidade < 10,
+  );
+
+  // Listar Produto
+  return produtosFiltrados;
+}
+
+function top5Estoque() {
+  // Criar uma cópia do array
+  const copiaProdutos = [...produtos];
+
+  // Percorrer todos os produtos, calcular o valor do estoque
+  // (preço × quantidade) e cria uma nova propriedade
+  // chamada valorEstoque em cada produto
+
+  copiaProdutos.forEach((produto) => {
+    produto.valorEstoque = produto.preco * produto.quantidade;
+  });
+
+  // Ordenar
+  copiaProdutos.sort((a, b) => {
+    return b.valorEstoque - a.valorEstoque;
+  });
+
+  // Pegar os 5 primeiros
+  return copiaProdutos.slice(0, 5);
+}
+
+function maiorValorEstoque() {
+  // Criar cópia do array
+  const copiaProdutos = [...produtos];
+
+  // Percorrer todos os produtos, calcular o valor do estoque
+  // e criar a propriedade valorEstoque
+  copiaProdutos.forEach((produto) => {
+    produto.valorEstoque = produto.preco * produto.quantidade;
+  });
+
+  // Ordenar do maior valor para o menor
+  copiaProdutos.sort((a, b) => {
+    return b.valorEstoque - a.valorEstoque;
+  });
+
+  // Retornar o primeiro produto
+  return copiaProdutos[0];
+}
+
+function valorPorCategoria() {
+  // Objeto vazio para guardar as categorias
+  const categorias = {};
+
+  // Percorrer todos os produtos
+  produtos.forEach((produto) => {
+    // Calcular valor do estoque
+    const valorEstoque = produto.preco * produto.quantidade;
+
+    // Pegar categoria do produto
+    const categoria = produto.categoria;
+
+    // Se a categoria já existir:
+    if (categorias[categoria]) {
+      categorias[categoria] += valorEstoque;
+    } else {
+      // Se não existir, criar categoria
+      categorias[categoria] = valorEstoque;
+    }
+  });
+
+  return categorias;
+}
+
+export {
+  listarProdutos,
+  listarPorCategoria,
+  calcularEstoqueProduto,
+  listarPorId,
+  listarPorNome,
+  listarEstoqueBaixo,
+  top5Estoque,
+  maiorValorEstoque,
+  valorPorCategoria,
+};
--- index.js
+++ index.js
@@ -1,53 +1,72 @@
-import express from 'express'
+import express from "express";
 
-import { listarProdutos, listarPorCategoria, calcularEstoqueProduto, listarPorId, listarPorNome } from './bd/servicos.js'
+import {
+  listarProdutos,
+  listarPorCategoria,
+  calcularEstoqueProduto,
+  listarPorId,
+  listarPorNome,
+  listarEstoqueBaixo,
+  top5Estoque,
+  maiorValorEstoque,
+  valorPorCategoria,
+} from "./bd/servicos.js";
 
+const app = express();
 
-const app = express()
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+app.get("/produtos", (req, res) => {
+  let produtos = listarProdutos();
+  res.json(produtos);
+});
 
-app.get('/produtos', (req, res) => {
-   let produtos = listarProdutos()
-   res.json(produtos)
-})
+app.get("/produtosCategoria/:categoria", (req, res) => {
+  let { categoria } = req.params;
+  let produtosFiltrados = listarPorCategoria(categoria);
+  res.json(produtosFiltrados);
+});
 
-app.get('/produtosCategoria/:categoria', (req, res) => {
-   let {categoria} = req.params
-   let produtosFiltrados = listarPorCategoria(categoria)
-   res.json(produtosFiltrados)
-})
-
-app.get('/estoque/:id', (req, res) => {
-
-  let { id } = req.params
-  let valorEstoque = calcularEstoqueProduto(id)
+app.get("/estoque/:id", (req, res) => {
+  let { id } = req.params;
+  let valorEstoque = calcularEstoqueProduto(id);
   res.json({
-      valorEstoque
-  })
+    valorEstoque,
+  });
+});
 
-})
+app.get("/produtoId/:id", (req, res) => {
+  let { id } = req.params;
+  let produto = listarPorId(id);
+  res.json(produto);
+});
 
-app.get('/produtoId/:id', (req, res) => {
+app.get("/produtoNome/:nome", (req, res) => {
+  let { nome } = req.params;
+  let produto = listarPorNome(nome);
+  res.json(produto);
+});
 
-  let { id } = req.params
-  let produto = listarPorId(id)
-  res.json(produto)
+app.get("/estoqueBaixo", (req, res) => {
+  let produtos = listarEstoqueBaixo();
+  res.json(produtos);
+});
 
-})
+app.get("/top5Estoque", (req, res) => {
+  let produtos = top5Estoque();
+  res.json(produtos);
+});
 
-app.get('/produtoNome/:nome', (req, res) => {
+app.get("/maiorValorEstoque", (req, res) => {
+  let produto = maiorValorEstoque();
+  res.json(produto);
+});
 
-  let { nome } = req.params
-  let produto = listarPorNome (nome)
-  res.json(produto)
-
-})
-
+app.get("/valorCategoria", (req, res) => {
+  let categorias = valorPorCategoria();
+  res.json(categorias);
+});
 
 app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000')
-})
-
+  console.log("Server is running on http://localhost:3000");
+});
