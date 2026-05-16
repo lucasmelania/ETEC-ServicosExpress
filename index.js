39bf33a3025f6f9b181f8c87f16b37fe9f92f3ff
lucasmelania <contact.lucasmelania@gmail.com>
2026-05-15 23:21:53 -0300

    6 commit

--- index.js
+++ index.js
@@ -67,6 +67,8 @@ app.get("/valorCategoria", (req, res) => {
   res.json(categorias);
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
